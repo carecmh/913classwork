9db34b8001dcc214873ba3822c360acfbbe88125
CAROLINE HAMON <CMH20D@FSUEDU>
2023-09-13 11:32:40 -0400

    Ping pong

--- sketch.js
+++ sketch.js
@@ -1,7 +1,16 @@
+let x = 200;
+let speed = 4;
+let diameter = 40;
+
 function setup() {
   createCanvas(400, 400);
+
 }
 
 function draw() {
-  background(125);
+  background(123);
+  ellipse(x, height/2, diameter);
+if (x >= width || x <= 0) {speed = speed * -1; 
+} x += speed
+
 }
