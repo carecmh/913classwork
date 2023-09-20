f70ffde14b2e427531ad16a430cfcfde5f7bbad0
CAROLINE HAMON <CMH20D@FSUEDU>
2023-09-19 23:08:26 -0400

    no loop

--- sketch.js
+++ sketch.js
@@ -2,15 +2,23 @@ let x = 200;
 let speed = 4;
 let diameter = 40;
 
+
 function setup() {
   createCanvas(400, 400);
+ 
 
 }
 
 function draw() {
   background(123);
   ellipse(x, height/2, diameter);
-if (x >= width || x <= 0) {speed = speed * -1; 
-} x += speed
-
+  if (x >= width || x <= 0) {
+    speed = speed * -1; 
+  } 
+  x += speed;
 }
+
+function mousePressed() {
+  noLoop()
+}
+
