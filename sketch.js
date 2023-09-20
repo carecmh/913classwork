d2a747c518afeef2070d643ab314d0a8706c5d51
CAROLINE HAMON <CMH20D@FSUEDU>
2023-09-19 23:41:19 -0400

    Mouse 4

--- sketch.js
+++ sketch.js
@@ -1,7 +1,8 @@
-let x = 200;
-let speed = 4;
+let ballx = 200;
+let ballspeed = 4;
+let rectx = 0;
 let diameter = 40;
-
+let shouldMove = true;
 
 function setup() {
   createCanvas(400, 400);
@@ -11,18 +12,19 @@ function setup() {
 
 function draw() {
   background(123);
-  ellipse(x, height/2, diameter);
-  if (x >= width || x <= 0) {
-    speed = speed * -1; 
-  } 
-  x += speed;
+  if (ballx > width || ballx < 0) {
+    ballspeed = ballspeed * -1;
+  }
+  if(shouldMove){
+    ballx = ballx + ballspeed;
+  }
+  ellipse(ballx, height/2, 30)
+  
+  rectx = rectx +1
+  rect(rectx, 100, 50, 50)
 }
 
-function mousePressed() {
-  noLoop();
-}
-
-function mouseReleased() {
-  loop();
+function mouseClicked() {
+shouldMove = ! shouldMove
 }
 
